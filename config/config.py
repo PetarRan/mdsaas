8b68bdd06db3b6b1ae63749f1b62f47c61220655
sveotac <strahinja.aleksic@hotmail.com>
2023-11-08 00:52:11 +0100

    added instructions.md

--- config/config.py
+++ config/config.py
@@ -1,4 +1,5 @@
 class Config:
+    NEWS_API_KEY = 'd5f82d67-ecbd-4d93-9f3d-0a665976c0d9'
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:mojstrale02@postgres:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'your_secret_key'
